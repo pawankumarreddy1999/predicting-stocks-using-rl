49e26e23f94d5a75ee4e6339723b1a1e8b499f5e
Pavan Kumar Reddy Elluru <78949569+pawankumarreddy1999@users.noreply.github.com>
2021-06-05 12:47:40 +0530

    Update stockEnv.py

--- stock_env/envs/stockEnv.py
+++ stock_env/envs/stockEnv.py
@@ -1,56 +1,48 @@
 from gym import Env
 from gym.spaces import Discrete, Box
+from gym import error, spaces, utils
+
 import numpy as np
 import random
 import pandas as pd
 import gym
 
-df = pd.read_csv('C:\\Users\\PAVAN KUMAR REDDY\\Downloads\\3files\\HDFC.csv')
-df = df.sort_values('Date')
-#print(df.head())
 
 class StockEnv(Env):    # Inherite the methods and properties from openai gym
-    df = pd.read_csv('C:\\Users\\PAVAN KUMAR REDDY\\Downloads\\3files\\HDFC.csv')
-    df = df.sort_values('Date')
-
     def __init__(self):
-
-        self.df = df
+        self.f = 'C:\\Users\\PAVAN KUMAR REDDY\\Downloads\\3files\\HDFC.csv'
+        self.df = pd.DataFrame(pd.read_csv(self.f))
+        self.result=[]
 
         # Actions we can take, buy, hold, sell
         self.action_space = Discrete(3)
-        self.observation_space = Box(low=0, high=1,shape=(5,1),dtype=np.float32)
+        self.observation_space = spaces.Discrete(self.df.shape[0])
         self.current_step=0
-
+        self.done=False
     def step(self, action):
-        result=[]
-        if action<1: #buy
-            if (self.df.loc[self.current_step, 'Open']< self.df.loc[self.current_step, 'Price']):
-                result.append(self.df.loc[self.current_step, 'Date'])
+        if(self.current_step<len(self.df)):
+            if action<1: #buy
+                #print('Open: {}'.format(self.df.loc[self.current_step, 'Open']),'Price : {}'.format(self.df.loc[self.current_step, 'Price']))
+                if (self.df.loc[self.current_step, 'Open']< self.df.loc[self.current_step, 'Price']):
+                    self.result.append(self.df.loc[self.current_step, 'Date'])
+                    #print('step number is: {}'.format(self.current_step),'action is: {}'.format(action),'Date: {}'.format(self.df.loc[self.current_step, 'Date']),'BUY')
+            if action>1: #sell
+                #print('Open: {}'.format(self.df.loc[self.current_step, 'Open']),'Price : {}'.format(self.df.loc[self.current_step, 'Price']))
+                if (self.df.loc[self.current_step, 'Open']> self.df.loc[self.current_step, 'Price']):
+                    self.result.append(self.df.loc[self.current_step, 'Date'])
+                    #print('step number is: {}'.format(self.current_step),'action is: {}'.format(action),'Date: {}'.format(self.df.loc[self.current_step, 'Date']),'SELL')
 
-        if action>1: #sell
-            if (self.df.loc[self.current_step, 'Open']> self.df.loc[self.current_step, 'Price']):
-                result.append(self.df.loc[self.current_step, 'Date'])
-
-        if(self.current_step==len(df)):
-            done=True
+        if(self.current_step==len(self.df)):
+            self.done=True
         else:
-            done=False
+            self.done=False
         self.current_step+=1
-        info={}
-        return self.current_step,result,done,info
+        return self.result, self.current_step
     def reset(self):
         self.current_step=0
-        return self.current_step
+        self.result=[]
+        self.done=False
+        return self.result,self.done
     def render(self):
         pass
 
-
-env=StockEnv()
-print(env.action_space)
-print(env.observation_space)
-done = False
-while not done:
-        #env.render()
-        action = env.action_space.sample()
-        _, result, done, info = env.step(action)
