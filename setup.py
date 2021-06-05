af2b5b9af866b90143971b8ad362e88da97da23d
Pavan Kumar Reddy Elluru <78949569+pawankumarreddy1999@users.noreply.github.com>
2021-06-05 08:52:18 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='stock_env',
+      version='1.2.0',
+      install_requires=[
+          'gym>=0.12.5',
+          'numpy>=1.16.4',
+          'pandas>=0.24.2',
+          'matplotlib>=3.1.1'
+      ]#And any other dependencies required
+)
