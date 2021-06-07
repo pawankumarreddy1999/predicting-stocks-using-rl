925d4703a5ee700ca040660803ccddf6bd39050c
Pavan Kumar Reddy Elluru <78949569+pawankumarreddy1999@users.noreply.github.com>
2021-06-07 16:03:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
 import gym
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import A2C,PPO2
+import numpy as np
 import stock_env
-env=gym.make('stock-v0')
-while not env.done:
 
+
+env=gym.make('stock1-v0')
+''''
+while True:
     #print(env.action_space)
     #print(env.observation_space)
     action = env.action_space.sample()
-    result,step = env.step(action)
-    print(result)
-    if env.done:
+    #print(action)
+    ob,result,done,info= env.step(action)
+    #print(result)
+    if done:
         break
+#print("BUY",result_buy)
+print(result)
+
+
+
+#env_maker = lambda: gym.make('stock1-v0')
+#env = DummyVecEnv([env_maker])
+'''
+
+
+
+model = PPO2('MlpPolicy', env, verbose=1)
+model.learn(total_timesteps=100)
+#model=PPO2.load('ppo2_stock')
+print(model)
+obs = env.reset()
+count=5
+print(obs)
+while True:
+    action, _states = model.predict(obs)
+    #print(action)
+    obs, rewards, dones = env.step(action[4])
+    #count-=1
+    if dones:
+        break
+print(" Reward is:{}".format(rewards[0]))
+print("Results for buy: {}".format(rewards[1]))
+print("Results for sell {}".format(rewards[2]))
